b42b72e707325ad28cbbfb056e266e001bc48883
jose alex <josealex41012@gmail.com>
2024-11-12 21:23:11 -0300

    enviando arquivos trabalhados monitoria dia 04/11/2024

---
+++ Projetos terceiro bimestre/js/aula-web-12.js
@@ -0,0 +1,14 @@
+ //alert("Hdllo, World!");
+ //console.log("teste");
+ var nome;
+ var caixa;
+ function alterarCor() {
+    document.getElementById("titulo").style.backgroundColor = "red";
+ }
+ nome = prompt("Digite o seu nome: ");
+ alert(nome);
+ if (nome == "alex") {
+    alert("O meu nome está correto!");
+ } else {
+    alert("O meu nome não está cooreto!");
+ }
